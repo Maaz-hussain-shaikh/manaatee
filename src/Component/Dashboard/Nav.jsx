48a3b13836e6503029d6d6a0ffae5c580834ede9
Maaz <maazratlam5@gmail.com>
2024-09-28 14:02:51 +0530

    small bug

--- src/Component/Dashboard/Nav.jsx
+++ src/Component/Dashboard/Nav.jsx
@@ -3,30 +3,30 @@ import { Link, useNavigate } from 'react-router-dom'
 
 
 const Nav = ({ onToggle }) => {
-const locate=useNavigate()
-  const name  = sessionStorage.getItem('username')
+  const locate = useNavigate()
+  const name = sessionStorage.getItem('username')
 
-  const onlogout =()=>{
-locate("/");
-sessionStorage.clear();
+  const onlogout = () => {
+    locate("/");
+    sessionStorage.clear();
   }
   return (
     <>
       <div className="fixed w-full flex items-center justify-between h-14 text-white z-10">
-          <div className="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-blue-800 dark:bg-gray-800 border-none">
-            <img className="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg" alt='domi' />
-            <span className="hidden md:block">{name}</span>
+        <div className="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-blue-800 dark:bg-gray-800 border-none">
+          <img className="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg" alt='domi' />
+          <span className="hidden md:block">{name}</span>
+        </div>
+        <div className="flex justify-between items-center h-14 bg-blue-800 dark:bg-gray-800 header-right">
+          <div className="bg-white rounded flex items-center w-full max-w-xl mr-4 p-2 shadow-sm border border-gray-200">
+            <button className="outline-none focus:outline-none">
+              <svg className="w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </button>
+            <input type="search" name="" id="" placeholder="Search" className="w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent" />
           </div>
-          <div className="flex justify-between items-center h-14 bg-blue-800 dark:bg-gray-800 header-right">
-            <div className="bg-white rounded flex items-center w-full max-w-xl mr-4 p-2 shadow-sm border border-gray-200">
-              <button className="outline-none focus:outline-none">
-                <svg className="w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-              </button>
-              <input type="search" name="" id="" placeholder="Search" className="w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent" />
-            </div>
-            <ul className="flex items-center">
-              <li>
-                {/* <button
+          <ul className="flex items-center">
+            <li>
+              {/* <button
                   aria-hidden="true"
 
                   className="group p-2 transition-colors duration-200 rounded-full shadow-md bg-blue-200 hover:bg-blue-200 dark:bg-gray-50 dark:hover:bg-gray-200 text-gray-900 focus:outline-none"
@@ -39,31 +39,31 @@ sessionStorage.clear();
                 
                 </button> if we need notification here we can use this 
                 */}
-              </li>
-              <li>
-                <Link to="/Dash" className="flex items-center ml-4 hover:text-blue-100" onClick={onToggle}>
-                  <span className="inline-flex justify-center items-center mr-1">
-                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
-                  </span>
-                  Add Operator
-                </Link>
-              </li>
-              <li>
+            </li>
+            <li>
+              <div className="flex items-center ml-4 hover:text-blue-100" onClick={onToggle}>
+                <span className="inline-flex justify-center items-center mr-1">
+                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+                </span>
+                Add Operator
+              </div>
+            </li>
+            <li>
 
-                <div className="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
-              </li>
+              <div className="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
+            </li>
 
-              <li>
-                <Link to="/Dash" className="flex items-center mr-4 hover:text-blue-100" onClick={onlogout}>
-                  <span className="inline-flex mr-1">
-                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
-                  </span>
-                  Logout
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <li>
+              <Link to="/" className="flex items-center mr-4 hover:text-blue-100" onClick={onlogout}>
+                <span className="inline-flex mr-1">
+                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+                </span>
+                Logout
+              </Link>
+            </li>
+          </ul>
         </div>
+      </div>
     </>
   )
 }
